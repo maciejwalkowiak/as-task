08036707138279117d29688cc7a644a320f27593
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2015-07-28 21:12:38 +0200

    Task 004 - Implemented StringCompressor
    Configured Maven to use Java 8

---
+++ task-004/src/main/java/com/github/maciejwalkowiak/as/task004/StringCompressor.java
@@ -0,0 +1,53 @@
+package com.github.maciejwalkowiak.as.task004;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Service
+public class StringCompressor {
+    public String compress(String uncompressed) {
+        List<CharCounter> result = new ArrayList<>();
+
+        // finds last item in list
+        Function<List<CharCounter>, CharCounter> findLastItem = list -> result.get(result.size() - 1);
+
+        for (Character c : uncompressed.toCharArray()) {
+            if (result.isEmpty() || !findLastItem.apply(result).isSameAs(c)) {
+                result.add(new CharCounter(c));
+            } else {
+                findLastItem.apply(result).increment();
+            }
+        }
+
+        String compressed = result.stream()
+            .map(CharCounter::asString)
+            .collect(Collectors.joining(""));
+
+        return compressed.length() >= uncompressed.length() ? uncompressed : compressed;
+    }
+
+    private static class CharCounter {
+        private final char character;
+        private int count = 1;
+
+        public CharCounter(char character) {
+            this.character = character;
+        }
+
+        public void increment() {
+            this.count++;
+        }
+
+        public boolean isSameAs(char c) {
+            return character == c;
+        }
+
+        public String asString() {
+            return character + "" + (count > 1 ? count : "");
+        }
+    }
+}
